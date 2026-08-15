0fb63528d024aec78aca91e877c34797f250f359
Aditya Sahani <166254189+adityasahani001@users.noreply.github.com>
2026-08-15 23:57:33 +0530

    1929. Concatenation of Array

---
+++ Day-1/Homework/LC1929.java
@@ -0,0 +1,30 @@
+// 1929. Concatenation of Array
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        
+        return ans;
+    }
+}
+
+
+// Alternative Java Built-in Approach
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        
+        System.arraycopy(nums, 0, ans, 0, n);
+        System.arraycopy(nums, 0, ans, n, n);
+        
+        return ans;
+    }
+}
