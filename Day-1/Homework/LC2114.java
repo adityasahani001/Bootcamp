24879ef7413fc99534b6072107f8065fc9eff799
Aditya Sahani <166254189+adityasahani001@users.noreply.github.com>
2026-08-17 23:39:54 +0530

    2114. Maximum Number of Words Found in Sentences

---
+++ Day-1/Homework/LC2114.java
@@ -0,0 +1,19 @@
+// 2114. Maximum Number of Words Found in Sentences
+
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int maxWords = 0;
+        
+        for (String sentence : sentences) {
+            int spaces = 0;
+            for (int i = 0; i < sentence.length(); i++) {
+                if (sentence.charAt(i) == ' ') {
+                    spaces++;
+                }
+            }
+            maxWords = Math.max(maxWords, spaces + 1);
+        }
+        
+        return maxWords;
+    }
+}
