3ff2304961d047ddf5d14a073992abae0cd92acb
Aditya Sahani <166254189+adityasahani001@users.noreply.github.com>
2026-08-19 00:02:28 +0530

    289. Game of Life

--- Day-3/Homework/LC289.java
+++ Day-3/Homework/LC289.java
@@ -1,26 +1,40 @@
 // 289. Game of Life
 
 class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
+    public void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
         
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+        int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
+        
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                int liveNeighbors = 0;
+                
+                for (int i = 0; i < 8; i++) {
+                    int nr = r + dr[i];
+                    int nc = c + dc[i];
+                    
+                    if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
+                        if (board[nr][nc] == 1 || board[nr][nc] == 2) {
+                            liveNeighbors++;
+                        }
+                    }
+                }
+                
+                if (board[r][c] == 1 && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    board[r][c] = 2;
+                }
+                else if (board[r][c] == 0 && liveNeighbors == 3) {
+                    board[r][c] = 3;
+                }
             }
         }
         
-        for (int i = 0; i < n; i++) {
-            int left = 0;
-            int right = n - 1;
-            while (left < right) {
-                int temp = matrix[i][left];
-                matrix[i][left] = matrix[i][right];
-                matrix[i][right] = temp;
-                left++;
-                right--;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                board[r][c] %= 2;
             }
         }
     }
