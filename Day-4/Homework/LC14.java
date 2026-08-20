d8954777459d58652f407a290b90d2b91aa93f9e
Aditya Sahani <166254189+adityasahani001@users.noreply.github.com>
2026-08-21 01:51:15 +0530

    14. Longest Common Prefix

---
+++ Day-4/Homework/LC14.java
@@ -0,0 +1,27 @@
+// 14. Longest Common Prefix
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs == null || strs.length == 0)
+            return "";
+
+        String first = strs[0];
+        for (int i = 0; i < first.length(); i++) {
+            char ch = first.charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != ch) {
+                    return first.substring(0, i);
+                }
+            }
+        }
+        return first;
+    }
+}
+
+
+// TC : O(n × m)
+//  n = number of strings
+// m = length of the shortest string
+
+// SC: O(1)
